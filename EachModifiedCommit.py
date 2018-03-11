60cf24b780799b86bb84b0b32d28f10538b2d3a7
Aastha Maheshwari <am07ruler@gmail.com>
2018-03-11 16:04:38 -0700

    Add files via upload
    For each commit, all the added and modified files in saving in separate file.

--- EachModifiedCommit.py
+++ EachModifiedCommit.py
@@ -1,32 +1,179 @@
 
 fp = open('newAMfiles.txt','r')
 
-line = fp.readline()
-#line = fp.readline()
+for i, line in enumerate(fp):
+    if i == 0:
+        fo = open('54.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 1:
+        fo = open('52.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 2:
+        fo = open('50.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 3:
+        fo = open('49.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 4:
+        fo = open('48.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 5:
+        fo = open('46.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 6:
+        fo = open('45.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 7:
+        fo = open('44.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 8:
+        fo = open('43.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 9:
+        fo = open('39.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 10:
+        fo = open('36.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 11:
+        fo = open('35.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 12:
+        fo = open('30.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 13:
+        fo = open('28.txt', 'w')
+        fo.write(line)
+        fo.close()
 
 
-# for line in lines:
-#     if(line.startwith('A')|line.startwith('M')):
-#         filename = 1
-#     else:
-#         filename = 0
-#
-# files = {}
-# for key in files:
-#     files[key] == 1
+    elif i == 14:
+        fo = open('27.txt', 'w')
+        fo.write(line)
+        fo.close()
 
 
-while(line):
-    ret = " "
-    ret += line
+    elif i == 15:
+        fo = open('26.txt', 'w')
+        fo.write(line)
+        fo.close()
 
-    fo = open('54.txt', 'w')
-    fo.write(ret)
-    fo.close()
+    elif i == 16:
+        fo = open('22.txt', 'w')
+        fo.write(line)
+        fo.close()
 
+    elif i == 17:
+        fo = open('20.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 18:
+        fo = open('19.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 19:
+        fo = open('18.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 20:
+        fo = open('14.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+
+    elif i == 21:
+        fo = open('13.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 22:
+        fo = open('12.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 23:
+        fo = open('10.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 24:
+        fo = open('5.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+
+    elif i == 25:
+        fo = open('4.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 26:
+        fo = open('3.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i == 27:
+        fo = open('2.txt', 'w')
+        fo.write(line)
+        fo.close()
+
+    elif i > 27:
+        break
 
 
 fp.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # for key in files
 # files[key] == 1
