5ff3b823a2dd9db7f3dbbc7e55b0a4161630268d
Aastha Maheshwari <am07ruler@gmail.com>
2018-05-19 02:06:54 -0700

    Add files via upload

---
+++ nameColor5.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nameColor5: MonoBehaviour {
+
+	public Text display_Filename_log;
+
+	void Start () {
+
+		display_Filename_log = GameObject.Find ("Canvas").GetComponentInChildren<Text> ();
+
+
+	}
+
+
+	void OnMouseOver()
+	{
+
+		display_Filename_log.text = "Screen Shot 2017-11-08 at 6.14.30 PM.png";
+	}
+
+	// Update is called once per frame
+	void OnMouseExit(){
+
+
+		display_Filename_log.text = " ";
+
+	}
+}
---
+++ nameDisplay1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nameDisplay1 : MonoBehaviour {
+
+	public Text display_Filename_vs;
+
+	void Start () {
+
+
+		//display_Filename_CB = GameObject.Find ("Canvas").GetComponentInChildren<Text> ();
+		display_Filename_vs = GameObject.Find ("Canvas").GetComponentInChildren<Text> ();
+		//display_Filename_images = GameObject.Find ("Canvas").GetComponentInChildren<Text> ();
+
+
+		//display_Filename = transform.localScale.Scale ();
+
+
+	}
+
+
+	void OnMouseOver()
+	{
+		//display_Filename_CB.text = "CBSoftware HTML: CSS Scripts";
+		display_Filename_vs.text = ".vs";
+		//display_Filename_images.text = "Images";
+	}
+
+	// Update is called once per frame
+	void OnMouseExit(){
+
+		//display_Filename_CB.text = " ";
+		display_Filename_vs.text = " ";
+		//display_Filename_vs.text = " ";
+
+	}
+}
